410f5e64506314d0e78ad5dd6bbe0e3580e8b208
Oleg <oleg_09-95@mail.ru>
2025-11-10 19:17:44 +0300

    Видео #7 Поднято состояние корзины и передано в в необходимые страницы

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -5,14 +5,25 @@ import {CheckoutPage} from "./pages/checkout/CheckoutPage.jsx";
 import {OrdersPage} from "./pages/OrdersPage.jsx";
 import {TrackingPage} from "./pages/TrackingPage.jsx";
 import {NotFoundPage} from "./pages/NotFoundPage.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/cart-items')
+            .then(response => {
+                setCart(response.data);
+            })
+    }, [])
 
   return (
     <>
         <Routes>
-            <Route index element={<HomePage />} />
-            <Route path="checkout" element={<CheckoutPage />} />
+            <Route index element={<HomePage cart={cart}/>} />
+            <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
             <Route path="orders" element={<OrdersPage />} />
             <Route path="tracking" element={<TrackingPage />} />
             <Route path="*" element={<NotFoundPage />} />
